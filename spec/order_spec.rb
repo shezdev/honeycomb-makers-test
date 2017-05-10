bdd4cf174b494be51b161f91f5355bb169a93e5c
Shereen <shereenfinney@gmail.com>
2017-05-10 21:26:27 +0100

    UPDATE ORDER SPEC     Create :order as described subject     Add describe, context and it blocks     Unit test to check Order is initialised with a material

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,5 +1,17 @@
 require "Order"
 
 describe "Order" do
+  subject(:order) {described_class.new}
+
+  describe "#initialize" do
+    context "when created" do
+      it "has an advertising material object" do
+        material = Material.new('WNP/SWCL001/010')
+        order = Order.new(material)
+        expect {(order.material.identifier).to eq(WNP/SWCL001/010)}
+      end
+    end
+  end
+
 
 end
