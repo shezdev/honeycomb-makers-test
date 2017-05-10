38037ccc7fe1085d480efbd6d777eda59088bffc
Shereen <shereenfinney@gmail.com>
2017-05-10 21:43:04 +0100

    REFACTOR INITIALIZE TEST IN ORDER SPEC
    Replace material object instance with let to DRY up code

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,13 +2,13 @@ require "Order"
 
 describe "Order" do
   subject(:order) {described_class.new}
+  let(:material) { Material.new 'WNP/SWCL001/010' }
 
   describe "#initialize" do
     context "when created" do
       it "has an advertising material object" do
-        material = Material.new('WNP/SWCL001/010')
         order = Order.new(material)
-        expect {(order.material.identifier).to eq(WNP/SWCL001/010)}
+        expect {(order.material.identifier).to eq('WNP/SWCL001/010')}
       end
     end
   end
