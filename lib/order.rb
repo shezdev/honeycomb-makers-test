38c6f2f529e56357c15d29c672142a9d9affd7b5
Shereen <shereenfinney@gmail.com>
2017-05-10 22:36:54 +0100

    PASSED DELIVERY #ADD UNIT TEST
    Create Add method in Order class
    Include items array in Initialize method of Order class
    Add attr_accessor for items

--- lib/order.rb
+++ lib/order.rb
@@ -1,8 +1,14 @@
 class Order
 
-  attr_accessor :material
+  attr_accessor :material, :items
 
   def initialize(material)
     self.material = material
+    self.items = []
   end
+
+  def add(broadcaster, delivery)
+    items << [broadcaster, delivery]
+  end
+
 end
