3ed565778b986148b4b415ca901aeef0985b9b57
Shereen <shereenfinney@gmail.com>
2017-05-10 16:28:45 +0100

    PASSED MATERIAL #INITIALIZE UNIT TEST
    Rename id to identifier in spec/material_spec.rb
    Add initialize method for Delivery class
    Add attr_accessor for name and price in Material class
    Tests pass in spec/material_spec.rb

--- spec/material_spec.rb
+++ spec/material_spec.rb
@@ -7,9 +7,9 @@ describe Material do
     context "when created" do
       it "has a clock number id" do
         material = Material.new('WNP/SWCL001/010')
-        expect { (material.id).to eq('WNP/SWCL001/010') }
+        expect { (material.identifier).to eq('WNP/SWCL001/010') }
       end
     end
   end
-  
+
 end
