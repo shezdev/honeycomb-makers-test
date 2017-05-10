caf9c9bdb43b8b91be0b7073ada02266adcaa528
Shereen <shereenfinney@gmail.com>
2017-05-10 22:15:12 +0100

    UPDATE ORDER SPEC WITH ADD UNIT TEST
    Create let for broadcaster and standard_delivery
    Create describe-context-it block
    Check if broadcaster and delivery is added to list array

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,8 +1,10 @@
 require "Order"
 
 describe "Order" do
-  subject(:order) {described_class.new}
+  subject(:order) { described_class.new }
   let(:material) { Material.new 'WNP/SWCL001/010' }
+  let(:broadcaster_1) { Broadcaster.new 1, 'Viacom' }
+  let (:standard_delivery) {Delivery.new :standard, 10.0}
 
   describe "#initialize" do
     context "when created" do
@@ -13,5 +15,15 @@ describe "Order" do
     end
   end
 
+  describe "#add" do
+    context "for the current order" do
+      it "adds the broadcaster and delivery method to an items list" do
+        order = Order.new(material)
+        list = []
+        order.add(broadcaster_1, standard_delivery)
+        expect(list).not_to be.empty
+      end
+    end
+  end
 
 end
