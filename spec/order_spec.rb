d48bf26c79d83c6b0009e07d5395384cbcd6e216
Shereen <shereenfinney@gmail.com>
2017-05-11 10:08:04 +0100

    UPDATE ORDER SPEC WITH GETSUBTOTAL METHOD
    Method should get the subtotal from items list
    Assign value to subtotal variable

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -4,6 +4,7 @@ describe "Order" do
   subject(:order) { described_class.new }
   let(:material) { Material.new 'WNP/SWCL001/010' }
   let(:broadcaster_1) { Broadcaster.new 1, 'Viacom' }
+  let(:broadcaster_2) { Broadcaster.new 2, 'Disney' }
   let (:standard_delivery) {Delivery.new :standard, 10.0}
 
   describe "#initialize" do
@@ -23,11 +24,22 @@ describe "Order" do
     context "for the current order" do
       it "adds the broadcaster and delivery method to an items list" do
         order = Order.new(material)
-        broadcaster_1 = "A"
-        delivery_1 = "B"
+        broadcaster_A = "A"
+        delivery_B = "B"
         items = []
-        order.add(broadcaster_1, standard_delivery)
+        order.add(broadcaster_A, delivery_B)
         expect { (items).to eq("A", "B") }
+      end
+    end
+  end
+
+  describe "#getSubtotal" do
+    context "returns the total cost of items before any discounts" do
+      it "should get the sub total from the items list" do
+        order = Order.new(material)
+        order.add(broadcaster_1, standard_delivery)
+        order.add(broadcaster_2, standard_delivery)
+        expect { (order.subtotal).to eq(20) }
       end
     end
   end
