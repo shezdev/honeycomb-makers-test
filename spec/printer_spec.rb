4724adaba45bbe7c77741eec936bd95fa937b4cf
Shereen <shereenfinney@gmail.com>
2017-05-11 13:46:38 +0100

    UPDATED #OUTPUT IN PRINTER SPEC
    Add test to print an order in it-do-end block

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -9,6 +9,31 @@ describe "Printer module" do
         test_order = order.new
         expect(test_order).to respond_to(:output).with(2).argument
       end
+
+      it "prints an order" do
+        test_order = order.new
+        items = ["Discovery", "express", 10.0]
+        subtotal = 10.0
+        expect(test_order.output(items, subtotal)).to eq(
+        "Order for WNP/SWCL001/010:
+        broadcaster          | delivery | price
+        -------------------- | -------- | --------
+        Discovery            | express  | $10.0
+        -------------------- | -------- | --------
+        Subtotal: $10.0")
+        expect(test_order.output(items, subtotal)).to eq(
+        "Order for WNP/SWCL001/010:
+        broadcaster          | delivery | price
+        -------------------- | -------- | --------
+        Discovery            | express  | $10.0
+        -------------------- | -------- | --------
+        Subtotal: $10.0")
+      end
+
+
+
+
+
     end
   end
 
