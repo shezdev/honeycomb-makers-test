77be47b8ee315f739df654026daccbf377e6fb38
Shereen <shereenfinney@gmail.com>
2017-05-11 13:54:30 +0100

    UPDATE #OUTPUT IN PRINTER SPEC
    Change output to respond to 3 arguments

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -7,13 +7,14 @@ describe "Printer module" do
     context "when invoked" do
       it "takes the items in the order and the order subtotal" do
         test_order = order.new
-        expect(test_order).to respond_to(:output).with(2).argument
+        expect(test_order).to respond_to(:output).with(3).argument
       end
 
       it "prints an order" do
         test_order = order.new
         items = ["Discovery", "express", 10.0]
         subtotal = 10.0
+        material_id = 'WNP/SWCL001/010'
         expect(test_order.output(items, subtotal)).to eq(
         "Order for WNP/SWCL001/010:
         broadcaster          | delivery | price
