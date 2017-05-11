da69549ae3acc1cd93027973ef71e7c642ff18d8
Shereen <shereenfinney@gmail.com>
2017-05-11 14:52:32 +0100

    UPDATE #OUTPUT IN PRINTER SPEC
    Add unit test to check the printer will print an order

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -2,7 +2,8 @@ require "Printer"
 
 describe "Printer module" do
   let(:order) { Class.new { include Printer } }
-
+  let(:broadcaster_3) { Broadcaster.new 3, 'Discovery' }
+  let (:express_delivery) {Delivery.new :express, 20}
   describe "#output" do
     context "when invoked" do
       it "takes the items in the order and the order subtotal" do
@@ -12,28 +13,14 @@ describe "Printer module" do
 
       it "prints an order" do
         test_order = order.new
-        items = ["Discovery", "express", 10.0]
-        subtotal = 10.0
+        broadcaster_3 = Broadcaster.new(3, 'Discovery')
+        # items = ["Discovery", "express", 10.0]
+        items = [[broadcaster_3, express_delivery]]
+        subtotal = 20.0
         material_id = 'WNP/SWCL001/010'
-        expect(test_order.output(items, subtotal)).to eq(
-        "Order for WNP/SWCL001/010:
-        broadcaster          | delivery | price
-        -------------------- | -------- | --------
-        Discovery            | express  | $10.0
-        -------------------- | -------- | --------
-        Subtotal: $10.0")
-        expect(test_order.output(items, subtotal)).to eq(
-        "Order for WNP/SWCL001/010:
-        broadcaster          | delivery | price
-        -------------------- | -------- | --------
-        Discovery            | express  | $10.0
-        -------------------- | -------- | --------
-        Subtotal: $10.0")
+        result = "Order for WNP/SWCL001/010:\nbroadcaster          | delivery | price   \n-------------------- | -------- | --------\nDiscovery            | express  | $20     \n-------------------- | -------- | --------\nSubtotal: $20.0"
+        expect(test_order.output(items, subtotal, material_id)).to eq(result)
       end
-
-
-
-
 
     end
   end
