cbcc4850e35f49607067a4671cf1fa29306da6a3
Shereen <shereenfinney@gmail.com>
2017-05-11 14:53:45 +0100

    UPDATE PRINTER MODULE
    Add body of output method
    Add frozen object called COLUMNS
    Add private method "output separator"

--- lib/printer.rb
+++ lib/printer.rb
@@ -1,6 +1,35 @@
 module Printer
 
-    def output(items, total_cost)
-    end
+  COLUMNS = {
+    broadcaster: 20, #name, width
+    delivery: 8,
+    price: 8
+  }.freeze
+
+  def output(items, subtotal, material_id)
+    [].tap do |result| #Yields 'result' to the block, and then returns 'result'
+      result << "Order for #{material_id}:"
+
+      result << COLUMNS.map { |name, width| name.to_s.ljust(width) }.join(' | ')
+      result << output_separator
+
+      items.each_with_index do |(broadcaster, delivery), index|
+        result << [
+          broadcaster.name.ljust(COLUMNS[:broadcaster]),
+          delivery.name.to_s.ljust(COLUMNS[:delivery]),
+          ("$#{delivery.price}").ljust(COLUMNS[:price])
+        ].join(' | ')
+      end
+
+      result << output_separator
+      result << "Subtotal: $#{subtotal}"
+    end.join("\n")
+  end
+
+  private
+
+  def output_separator
+    @output_separator ||= COLUMNS.map { |_, width| '-' * width }.join(' | ')
+  end
 
 end
