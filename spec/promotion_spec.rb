dab2ba4045d10c958aef4931e3a3751735d64218
Shereen <shereenfinney@gmail.com>
2017-05-11 22:56:59 +0100

    NEW TEST ADDED TO PROMOTION SPEC
    Add test to #over30get10Off" method in spec/promotion_spec.rb
    Test to check and calculate discount on order

--- spec/promotion_spec.rb
+++ spec/promotion_spec.rb
@@ -31,10 +31,21 @@ describe "Promotion" do
     describe "#over30get10Off" do
       context "when invoked" do
         it "receives the items from the order" do
-        test_order = order.new
-        expect(test_order).to respond_to(:over30get10Off).with(1).argument
+          test_order = order.new
+          expect(test_order).to respond_to(:over30get10Off).with(1).argument
+        end
+        it "checks and calculates discount on order" do
+          test_order = order.new
+          express_delivery = Delivery.new(:express, 20.0)
+          standard_delivery = Delivery.new(:standard, 10.0)
+          broadcaster_1 = Broadcaster.new(1, 'Viacom')
+          broadcaster_2 = Broadcaster.new(2, 'Disney')
+          broadcaster_3 = Broadcaster.new(3, 'Discovery')
+          broadcaster_4 = Broadcaster.new(3, 'Discovery')
+          items = [[broadcaster_1, express_delivery],[broadcaster_2, standard_delivery],[broadcaster_3, standard_delivery],[broadcaster_4, standard_delivery]]
+          expect(test_order.expDeliveryDownTo15(items)).to eq(45)
+        end
       end
-    end
 
     end
 
