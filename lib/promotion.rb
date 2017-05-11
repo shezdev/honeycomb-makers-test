45fac8be5919d2fd4d8e5dc7aa15f292f7f5ef0c
Shereen <shereenfinney@gmail.com>
2017-05-11 20:08:03 +0100

    CREATE PROMOTION MODULE
    Require "Promotion" in spec/promotion_spec.rb
    Create Promotion module in lib/promotion.rb

---
+++ lib/promotion.rb
@@ -0,0 +1,2 @@
+module Promotion
+end
--- spec/promotion_spec.rb
+++ spec/promotion_spec.rb
@@ -1,20 +1,22 @@
+require "Promotion"
+
 describe "Promotion" do
 
   describe "Promotion module" do
-    let(:order) { Class.new { include Promotion } }
+    let (:material) { Material.new 'WNP/SWCL001/010'}
+    let(:order) { Order.new(material) { include Promotion } }
     let(:broadcaster_1) { Broadcaster.new 1, 'Viacom' }
     let(:broadcaster_2) { Broadcaster.new 2, 'Disney' }
     let(:broadcaster_3) { Broadcaster.new 3, 'Discovery' }
     let (:standard_delivery) {Delivery.new :standard, 10.0}
     let (:express_delivery) {Delivery.new :express, 20.0}
-    let (:material) { Material.new 'WNP/SWCL001/010'}
 
     describe "#expDeliveryDownTo15" do
       context "when invoked" do
         it "checks and calculates discount on order" do
-        order1 = order.new(material)
-        order1.add(broadcaster_1, express_delivery)
-        order1.add(broadcaster_3, express_delivery)
+        # order1 = order.new(material)
+        order.add(broadcaster_1, express_delivery)
+        order.add(broadcaster_3, express_delivery)
         expect(order.expDeliveryDownTo15).to eq(10)
         end
       end
